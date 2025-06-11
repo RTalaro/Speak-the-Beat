3d045799b62400402612f21718fe2245bc92edd2
kljeung <kljeung@ucsc.edu>
2025-06-11 16:09:28 -0700

    Update TitleScene.js

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -4,25 +4,21 @@ class TitleScene extends Phaser.Scene {
     }
 
     preload() {
-        // Make sure to load the title image
         this.load.setPath('./assets/');
         this.load.image('title', 'title.png');
     }
 
     create() {
-        // Add the title background image centered on screen
         const centerX = this.scale.width / 2;
         const centerY = this.scale.height / 2;
 
         this.add.image(centerX, centerY, 'title').setOrigin(0.5);
 
-        // Overlay text
         this.add.text(centerX, centerY + 150, 'Press SPACE to start', {
             fontSize: '24px',
             fill: '#ffffff',
         }).setOrigin(0.5);
 
-        // Start main game scene when space is pressed
         this.input.keyboard.once('keydown-SPACE', () => {
             this.scene.stop();
             this.scene.start('loadScene');
